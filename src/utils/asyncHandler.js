37878caa5cbcf43b839784782ced3c0c9594c1bd
Jatan Dudeja <dudeja.jatan04@gmail.com>
2024-01-19 19:37:33 +0530

    user/register route added.

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,8 @@
-export {asyncHandler}
-
 const asyncHandler = (requestHandler) => {
     return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next))
-        .catch((err) => {
-            console.log("Error in asyncHandler is : ", err);
-        })
+        ((err) => next(err))
     }
 }
+
+export { asyncHandler }
